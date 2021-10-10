309c4dfe5a975b8925915d70a4d384f0905264ef
Pengyao Cui <pengyao.cui@cern.ch>
2021-10-10 09:44:14 +0800

    add v0 pp 7 data point

--- StrinJet/macro/f12_JE_LK.C
+++ StrinJet/macro/f12_JE_LK.C
@@ -2,8 +2,21 @@
 
 void f12_JE_LK(){
 //=============================================================================
-  auto hD(GetDataC("data/HEPData_2005.11120.root", 43)); 
-  auto gD = GetDataE("data/HEPData_2005.11120.root", 43); 
+  //auto hD(GetDataC("data/HEPData_2005.11120.root", 43)); 
+  //auto gD = GetDataE("data/HEPData_2005.11120.root", 43); 
+  Double_t dBin[] = {0.6, 1.6, 2.2, 2.8, 3.7, 5.0, 8.0, 12.}; Int_t nBin = sizeof(dBin)/sizeof(Double_t)-1;
+  Double_t dVal[] = {0.116, 0.190, 0.214, 0.227, 0.230, 0.235, 0.236};
+  Double_t dSta[] = {0.020, 0.020, 0.019, 0.019, 0.019, 0.022, 0.040};
+  Double_t dSys[] = {0.012, 0.021, 0.022, 0.023, 0.024, 0.024, 0.030};
+  TH1D* hD(new TH1D("hD", "", nBin, dBin));
+  TH1D* hE(new TH1D("hE", "", nBin, dBin));
+  for(int i=1; i<= nBin; i++){
+    hD->SetBinContent(i, dVal[i-1]); 
+    hD->SetBinError(i, dSta[i-1]); 
+    hE->SetBinContent(i, dVal[i-1]); 
+    hE->SetBinError(i, dSys[i-1]); 
+  }
+  auto gD = ConvHistogramToGraphErrors(hE);
 //=============================================================================
 
   TH1D* h1[3]; TH1D*h2[3]; TH1D* hR[3]; TGraph* g[3];
@@ -42,14 +55,16 @@ void f12_JE_LK(){
   g[0]->SetLineStyle(0);
   g[1]->SetLineStyle(1);
   g[2]->SetLineStyle(2);
-  //DrawHisto(hD, wcl[0], wmk[0], "same");
-  //DrawGraph(gD, wcl[0], "E2");
+  DrawHisto(hD, wcl[0], wmk[0], "same");
+  DrawGraph(gD, wcl[0], "E2");
   DrawGraph(g[0], wcl[0], "L");
   DrawGraph(g[1], wcl[1], "L");
   DrawGraph(g[2], wcl[2], "L");
 
-  auto leg(new TLegend(0.5, 0.70, 0.9, 0.92)); SetupLegend(leg);
+  auto leg(new TLegend(0.6, 0.70, 0.9, 0.92)); SetupLegend(leg);
   //leg->AddEntry(hD, "Data(Inclusive 13 TeV)",  "P")->SetTextSizePixels(24);
+  leg->AddEntry(hD, "Exp data",  "PF")->SetTextSizePixels(24);
+
   leg->AddEntry(g[0], "CR",  "L")->SetTextSizePixels(24);
   leg->AddEntry(g[1], "Rope",  "L")->SetTextSizePixels(24);
   leg->AddEntry(g[2], "CR+Rope",  "L")->SetTextSizePixels(24);
